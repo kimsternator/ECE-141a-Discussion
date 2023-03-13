be757e892b40974fbd8990971e8004ad616033b2
kimsternator <sskim@ucsd.edu>
2023-03-13 13:47:01 -0700

    Initial Commit

---
+++ Discussion8/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
+
+/*
+ * OOP + STL
+ * 		Encapsulation
+ * 		Inheritance
+ * 		Polymorphism
+General Polymorphism
+ 	CompileTime
+ 		Function overloading
+ 		Operator Overloading
+ 	Runtime
+ 		Virtual Function (Overriding)
+	What type is template?
+Solid Principles
+ 	SRP
+ 	Open-closed (interface)
+ 	Liskov
+ 	Interface Segregation
+ 	Dependency Inversion
+Design Patterns
+ 	Creational
+ 		Factory
+ 		Builder
+ 		Prototype
+ 		Singleton
+ 	Behavioral
+ 		Command
+ 		Chair of Responsibility
+ 		Iterator
+ 		Observer
+ 		Visitor
+ 	Structural
+ 		Adapter
+ 		Decorator
+ 		Proxy
+Communication Patterns
+ 	MVC
+ 	API (ReST)
+ 	Layers
+ 	Subscription
+Control/Flow Patterns
+ 	Outside In
+ 	Inside out
+(Avoiding) Anti-Patterns
+ 	Magic Numbers
+ 	const right
+ */
